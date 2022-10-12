2e46567f6b5b572457063e58587b2a3249f2d10a
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-12 21:01:56 +0100

    create register route

---
+++ Api/Controllers/AuthController.js
@@ -0,0 +1,9 @@
+// @desc POST Register
+// @route /api/users
+// access public
+const register=async(req,res)=>{
+    res.send('register')
+}
+module.exports={
+    register
+}
--- Api/server.js
+++ Api/server.js
@@ -2,4 +2,8 @@ const express=require('express')
 const dotenv=require('dotenv').config()
 const PORT=process.env.PORT || 5000
 const app=express()
+
+//Routes
+app.use('/api/v1/auth/',require('./Routes/AuthRoute'))
+
 app.listen(PORT,()=>console.log('Server running at the PORT '+PORT))
