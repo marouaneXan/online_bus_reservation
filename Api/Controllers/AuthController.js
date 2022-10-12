46c7c551279d60856312621713e3b279f54b521b
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-12 21:04:33 +0100

    create login route

--- Api/Controllers/AuthController.js
+++ Api/Controllers/AuthController.js
@@ -4,6 +4,13 @@
 const register=async(req,res)=>{
     res.send('register')
 }
+// @desc POST Register
+// @route /api/users
+// access public
+const login=async(req,res)=>{
+    res.send('login')
+}
 module.exports={
-    register
+    register,
+    login
 }
--- Api/Routes/AuthRoute.js
+++ Api/Routes/AuthRoute.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const router = express.Router();
-const {register}=require('../Controllers/AuthController')
+const {register,login}=require('../Controllers/AuthController')
 router.post('/register',register)
+router.post('/login',login)
 module.exports=router
