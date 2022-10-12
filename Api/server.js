5bc77f0628edbcab74e99e8a1b952b4cd441f075
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-12 17:00:31 +0100

    run the server at the port 5000

--- Api/server.js
+++ Api/server.js
@@ -2,3 +2,4 @@ const express=require('express')
 const dotenv=require('dotenv').config()
 const PORT=process.env.PORT || 5000
 const app=express()
+app.listen(PORT,()=>console.log('Server running at the PORT '+PORT))
