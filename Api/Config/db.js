0470e04a4b68bae6fa6992c86a6e519cfef58ab6
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-13 09:42:28 +0100

    add connectDb function to connect to mongodb

---
+++ Api/Config/db.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose')
+const connectDB=async ()=>{
+    try {
+        const conn=await mongoose.connect(process.env.MONGO_URI)
+        console.log(`mongoDB connected:${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+module.exports=connectDB
