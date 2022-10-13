612a6bc53d284407ce8a6acebc214158156be711
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-13 16:20:08 +0100

    check if the password is correct

--- Api/Controllers/AuthAdminController.js
+++ Api/Controllers/AuthAdminController.js
@@ -6,13 +6,24 @@ const Admin = require("../Models/Admin");
 // @route /api/v1/adminAuth/login
 // access public
 const login = asyncHandler(async (req, res) => {
-    const {email,password}=req.body
-    if(!email || !password){
-        res.status(400)
-        throw new Error('Please add all fields')
-    }
-    const admin=await Admin.findOne({email})
-
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const admin = await Admin.findOne({ email });
+  if (!admin) {
+    res.status(400);
+    throw new Error("Invalid admin data");
+  }
+  const isPasswordCorrect = await bcrypt.compare(
+    req.body.password,
+    admin.password
+  );
+  if (!isPasswordCorrect) {
+    res.status(400);
+    throw new Error("Password is incorrect");
+  }
 });
 module.exports = {
   login,
