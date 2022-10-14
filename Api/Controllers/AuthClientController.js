522c12edeb1fe6ac19dc4866e4a845e4d423d2a9
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-14 09:23:34 +0100

    init all fields by req.body

--- Api/Controllers/AuthClientController.js
+++ Api/Controllers/AuthClientController.js
@@ -5,10 +5,20 @@ const jwt = require("jsonwebtoken");
 //@desc POST register
 //@route /api/v1/clientAuth/register
 //@access private
-const register=asyncHandler(async(req,res)=>{
-    res.send('register client')
-})
-module.exports={
-    register
-}
-
+const register = asyncHandler(async (req, res) => {
+  const {
+    nom,
+    prenom,
+    genre,
+    email,
+    date_naissance,
+    tel,
+    adress,
+    ville,
+    nationalite,
+    password,
+  } = req.body;
+});
+module.exports = {
+  register,
+};
