755433679747bc79685e5fc06a769ce17df552a1
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-16 12:11:55 +0100

    add route for clientDetails

--- Api/Routes/ClientRoute.js
+++ Api/Routes/ClientRoute.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const router=express()
-const {deleteClient,getAllClients}=require('../Controllers/ClientController')
+const {deleteClient,getAllClients, clientDetails}=require('../Controllers/ClientController')
 router.delete('/:client_id',deleteClient)
 router.get('/',getAllClients)
+router.get('/:client_id',clientDetails)
 module.exports=router
