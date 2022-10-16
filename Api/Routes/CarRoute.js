29db57bb82706232bddf04f3efbe82ad36b6c34f
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-16 12:08:08 +0100

    add route for router.delete(/:car_id,deleteCar)

--- Api/Routes/CarRoute.js
+++ Api/Routes/CarRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const router=express()
-const {addCar, getAllCars,updateCar,deleteCar}=require('../Controllers/CarController')
+const {addCar, getAllCars,updateCar,deleteCar, carDetails}=require('../Controllers/CarController')
 router.post('/',addCar)
 router.get('/',getAllCars)
 router.put('/:car_id',updateCar)
 router.delete('/:car_id',deleteCar)
+router.get('/:car_id',carDetails)
 module.exports=router
