b14335c2883a042c2f3549410a3a6afaf9f748b9
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-16 10:54:47 +0100

    add route for deleteTrip

--- Api/Routes/TripRoute.js
+++ Api/Routes/TripRoute.js
@@ -1,7 +1,8 @@
 const express=require('express')
-const {getAllTrips, addTrip,updateTrip}=require('../Controllers/TripController')
+const {getAllTrips, addTrip,updateTrip,deleteTrip}=require('../Controllers/TripController')
 const router = express.Router();
 router.get('/',getAllTrips)
 router.post('/:car_id/:company_id',addTrip)
 router.put('/:trip_id',updateTrip)
+router.delete('/:trip_id',deleteTrip)
 module.exports=router
