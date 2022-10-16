0dd2881295dfca534f774b012198e7ae43fce4db
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-16 12:11:08 +0100

    create clientDetails function

--- Api/Controllers/ClientController.js
+++ Api/Controllers/ClientController.js
@@ -13,6 +13,14 @@ const deleteClient = asyncHandler(async (req, res) => {
   });
 });
 
+//@desc GET carDetails
+//@route /api/v1/clients/client_id
+//@access private
+const clientDetails = asyncHandler(async (req, res) => {
+  const client = await Client.find(req.params.client_id);
+  res.status(200).json(client);
+});
+
 //@desc GET getAllClients
 //@route /api/v1/clients
 //@access private
@@ -23,4 +31,5 @@ const getAllClients = asyncHandler(async (req, res) => {
 module.exports = {
   deleteClient,
   getAllClients,
+  clientDetails
 };
