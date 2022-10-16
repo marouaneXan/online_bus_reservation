a27da8f76494e9a6f9aac2c42ced55b0094496cd
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-16 12:01:40 +0100

    add route for updateCompany and deleteCompany

--- Api/Routes/CompanyRoute.js
+++ Api/Routes/CompanyRoute.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router = express.Router()
-const {addCompany,getAllCompanies}=require('../Controllers/CompanyController')
+const {addCompany,getAllCompanies,updateCompany,deleteCompany}=require('../Controllers/CompanyController')
 router.post('/',addCompany)
 router.get('/',getAllCompanies)
+router.put('/company_id',updateCompany)
+router.delete('/company_id',deleteCompany)
 module.exports=router
