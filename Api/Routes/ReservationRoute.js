45f78b878511d80e75bb83451a462ccbaa13f0bc
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-17 19:52:12 +0100

    protect routes for Reservation fonctionality

--- Api/Routes/ReservationRoute.js
+++ Api/Routes/ReservationRoute.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const { getAllReservations, makeReservation, cancelReservation } = require('../Controllers/ReservationController')
+const {protectClient}=require('../Middleware/ClientMiddleware')
+const {protectAdmin}=require('../Middleware/AdminMiddleware')
 const router = express.Router()
-router.get('/',getAllReservations)
-router.post('/:trip_id/:client_id',makeReservation)
-router.delete('/:reservation_id/:trip_id/:client_id',cancelReservation)
+router.get('/',protectAdmin,getAllReservations)
+router.post('/:trip_id/:client_id',protectClient,makeReservation)
+router.delete('/:reservation_id/:trip_id/:client_id',protectClient,cancelReservation)
 module.exports=router
