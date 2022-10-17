52a74b2af9524618349bd6ec08b5124b4a88e4d3
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-17 19:54:58 +0100

    protect routes for Trip fonctionality

--- Api/Routes/TripRoute.js
+++ Api/Routes/TripRoute.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const {getAllTrips, addTrip,updateTrip,deleteTrip, tripDetails}=require('../Controllers/TripController')
+const {protectAdmin}=require('../Middleware/AdminMiddleware')
 const router = express.Router();
-router.get('/',getAllTrips)
-router.post('/:car_id/:company_id',addTrip)
-router.put('/:trip_id',updateTrip)
-router.delete('/:trip_id',deleteTrip)
+router.get('/',protectAdmin,getAllTrips)
+router.post('/:car_id/:company_id',protectAdmin,addTrip)
+router.put('/:trip_id',protectAdmin,updateTrip)
+router.delete('/:trip_id',protectAdmin,deleteTrip)
 router.get('/:trip_id',tripDetails)
 module.exports=router
