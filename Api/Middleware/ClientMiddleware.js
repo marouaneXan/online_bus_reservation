fc8ab51c1ebf4fdce64581cb6bb9b02695836469
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-17 19:42:15 +0100

    create function protectClient to protect routes for client

---
+++ Api/Middleware/ClientMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Client = require("../Models/Client");
+const protectClient = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //Get Token from Header
+      token = req.headers.authorization.split(" ")[1];
+      //Verify Token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //Get Client From Token
+      req.client = await Client.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, No token");
+  }
+});
+module.exports = { protectClient };
