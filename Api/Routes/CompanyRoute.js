f69ad8d89edb637a4b4ae7b6a8f4130c53090ca0
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-17 19:47:47 +0100

    protect routes for Company fonctionality

--- Api/Routes/CompanyRoute.js
+++ Api/Routes/CompanyRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const router = express.Router()
 const {addCompany,getAllCompanies,updateCompany,deleteCompany}=require('../Controllers/CompanyController')
-router.post('/',addCompany)
-router.get('/',getAllCompanies)
-router.put('/:company_id',updateCompany)
-router.delete('/:company_id',deleteCompany)
+const {protectAdmin}=require('../Middleware/AdminMiddleware')
+router.post('/',protectAdmin,addCompany)
+router.get('/',protectAdmin,getAllCompanies)
+router.put('/:company_id',protectAdmin,updateCompany)
+router.delete('/:company_id',protectAdmin,deleteCompany)
 module.exports=router
