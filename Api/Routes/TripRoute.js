74526e540a18b0512c65ecc1e6efae83acf46b6f
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-18 15:11:57 +0100

    add route forgetAllTrips

--- Api/Routes/TripRoute.js
+++ Api/Routes/TripRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
-const {searchTrip, addTrip,updateTrip,deleteTrip, tripDetails}=require('../Controllers/TripController')
+const {searchTrip,getAllTrips, addTrip,updateTrip,deleteTrip, tripDetails}=require('../Controllers/TripController')
 const {protectAdmin}=require('../Middleware/AdminMiddleware')
 const router = express.Router();
-router.get('/',protectAdmin,searchTrip)
+router.get('/',searchTrip)
+router.get('/',protectAdmin,getAllTrips)
 router.post('/:car_id/:company_id',protectAdmin,addTrip)
 router.put('/:trip_id',protectAdmin,updateTrip)
 router.delete('/:trip_id',protectAdmin,deleteTrip)
