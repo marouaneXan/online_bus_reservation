fb15897718dec3eff816d0c437101b3a228c822f
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-18 21:18:10 +0100

    cancel reservation before 1hour departure time for bus

--- Api/Controllers/ReservationController.js
+++ Api/Controllers/ReservationController.js
@@ -21,7 +21,7 @@ const getAllReservations = asyncHandler(async (req, res) => {
 const getClientReservations = asyncHandler(async (req, res) => {
   const client_id = req.params.client_id;
   if (client_id) {
-    const reservations = await Reservation.find({client:client_id});
+    const reservations = await Reservation.find({ client: client_id });
     reservations.length
       ? res.status(200).json(reservations)
       : res.status(400).json({
@@ -64,8 +64,7 @@ const makeReservation = asyncHandler(async (req, res) => {
 //@route /api/v1/reservation/reservation_id/trip_id/client_id
 //@access public
 const cancelReservation = asyncHandler(async (req, res) => {
-  // const time_now = new Date().toLocaleTimeString();
-  // const date_now = new Date().toLocaleDateString();
+  const time_now = new Date().toLocaleTimeString();
   const reservation = await Reservation.findById(req.params.reservation_id);
   const trip = await Trip.findById(req.params.trip_id);
   const client = await Client.findById(req.params.client_id);
@@ -73,35 +72,45 @@ const cancelReservation = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Canceling reservation failed :(");
   }
-  reservation.remove();
-  const car = await Car.findById(trip.car);
-  await Car.findByIdAndUpdate(trip.car, {
-    nbr_places: car.nbr_places + 1,
-  });
-  res.status(200).json({
-    message: "Reservation canceling successfully",
-  });
-  // if (reservation.reservation_date > date_now) {
-  //   reservation.remove();
-  //   const car = await Car.findById(trip.car);
-  //   await Car.findByIdAndUpdate(trip.car, {
-  //     nbr_places: car.nbr_places + 1,
-  //   });
-  // } else if (
-  //   reservation.reservation_date === date_now &&
-  //   reservation.reservation_time - trip.departure_time > "01:00:00" &&
-  //   time_now - trip.departure_time > "01:00:00"
-  // ) {
-  //   reservation.remove();
-  //   const car = await Car.findById(trip.car);
-  //   await Car.findByIdAndUpdate(trip.car, {
-  //     nbr_places: car.nbr_places + 1,
-  //   });
-  // } else {
-  //   res.status(400);
-  //   throw new Error("You cannot cancel this reservation");
-  // }
+  const difference_time = diff(time_now, trip.departure_time);
+  if (reservation.reservation_date < trip.departure_date) {
+    reservation.remove();
+    const car = await Car.findById(trip.car);
+    await Car.findByIdAndUpdate(trip.car, {
+      nbr_places: car.nbr_places + 1,
+    });
+    res.status(200).json({ message: "Canceling reservation successffuly" });
+  } else if (
+    reservation.reservation_date === trip.departure_date &&
+    difference_time > "01:00:00"
+  ) {
+    reservation.remove();
+    const car = await Car.findById(trip.car);
+    await Car.findByIdAndUpdate(trip.car, {
+      nbr_places: car.nbr_places + 1,
+    });
+    res.status(200).json({ message: "Canceling reservation successffuly" });
+  } else {
+    res.status(400);
+    throw new Error("You cannot cancel this reservation");
+  }
 });
+
+// difference btween two times
+function diff(start, end) {
+  start = start.split(":");
+  end = end.split(":");
+  var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+  var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+  var diff = endDate.getTime() - startDate.getTime();
+  var hours = Math.floor(diff / 1000 / 60 / 60);
+  diff -= hours * 1000 * 60 * 60;
+  var minutes = Math.floor(diff / 1000 / 60);
+
+  return (
+    (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes
+  );
+}
 module.exports = {
   getAllReservations,
   makeReservation,
