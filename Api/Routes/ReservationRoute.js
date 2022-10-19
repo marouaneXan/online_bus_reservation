8cdd10b31bfacf62c2ca36f8b4ac53e2d0fc30e1
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-19 10:51:32 +0100

    create routes for getReservationDetails function

--- Api/Routes/ReservationRoute.js
+++ Api/Routes/ReservationRoute.js
@@ -1,10 +1,11 @@
 const express=require('express')
-const { getAllReservations, makeReservation, cancelReservation,getClientReservations } = require('../Controllers/ReservationController')
+const { getAllReservations, makeReservation, cancelReservation,getClientReservations, getReservationDetails } = require('../Controllers/ReservationController')
 const {protectClient}=require('../Middleware/ClientMiddleware')
 const {protectAdmin}=require('../Middleware/AdminMiddleware')
 const router = express.Router()
 router.get('/',protectAdmin,getAllReservations)
 router.get('/:client_id', protectClient,getClientReservations)
+router.get('/:reservation_id', protectAdmin,getReservationDetails)
 router.post('/:trip_id/:client_id',protectClient,makeReservation)
 router.delete('/:reservation_id/:trip_id/:client_id',protectClient,cancelReservation)
 module.exports=router
