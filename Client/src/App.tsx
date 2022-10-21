9af539ea1377f98d937ef96ec92b34e374d3a8a4
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-21 09:31:45 +0100

    first setup for react-router-dom and create a navbar component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Layouts/Navbar";
+import Register from "./Pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <h1></h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="register" element={<Register/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
