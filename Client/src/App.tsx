020dc40a5c3536ae9febcf232b211d4784e7c28d
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-21 09:21:08 +0100

    install all dependencies for client side

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1></h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
