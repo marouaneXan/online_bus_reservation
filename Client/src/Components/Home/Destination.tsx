a512f522aa1f516333573ee1468b90e748fd6810
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-22 14:12:13 +0100

    small change Destinations component

--- Client/src/Components/Home/Destination.tsx
+++ Client/src/Components/Home/Destination.tsx
@@ -8,7 +8,7 @@ const Destinations = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
         <h1 className='text-3xl md:text-4xl font-bold'>All-Inclusive Resorts</h1>
-        <p className='py-4 text-xl md:text-4xl font-semibold'>The Best cities</p>
+        <p className='py-4 text-xl md:text-xl'>The Best cities</p>
         <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
             <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={safi} alt="/" />
             <img className='w-full h-full object-cover' src={casablanca} alt="/" />
