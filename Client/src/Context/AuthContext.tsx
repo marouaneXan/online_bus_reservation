97c648915b6d70c7fb022e81f96929f1434cb5be
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-24 15:01:28 +0100

    importing useMemo and pass state to useMemo

--- Client/src/Context/AuthContext.tsx
+++ Client/src/Context/AuthContext.tsx
@@ -1,16 +1,42 @@
-import { createContext,useState } from "react";
+import { createContext, useState,useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext(null);
 
 const AuthContextProvider = ({ children }: any) => {
   const navigate = useNavigate();
   const [connected, setConnected] = useState(localStorage.getItem("logged"));
-  const [id, setId] = useState<string | null>(localStorage.getItem("client_id"));
+  const [id, setId] = useState<string | null>(
+    localStorage.getItem("client_id")
+  );
   const [success, setSuccess] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const name: string = "marouane";
-  const values = { name };
+  const values = useMemo(
+    () => ({
+      connected,
+      setConnected,
+      id,
+      setId,
+      loading,
+      setLoading,
+      success,
+      setSuccess,
+      error,
+      setError,
+    }),
+    [
+      connected,
+      setConnected,
+      id,
+      setId,
+      loading,
+      setLoading,
+      success,
+      setSuccess,
+      error,
+      setError,
+    ]
+  );
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 export default AuthContextProvider;
