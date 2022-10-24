2a78691d91519fdc7d85f27d9c708b1364861f1a
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-24 14:51:12 +0100

    import useNavigate

--- Client/src/Context/AuthContext.tsx
+++ Client/src/Context/AuthContext.tsx
@@ -1,8 +1,9 @@
 import {createContext} from 'react'
+import { useNavigate } from "react-router-dom";
 export const AuthContext=createContext(null)
 
 const AuthContextProvider=({children}:any)=>{
-    
+
     const name:string="marouane"
     const values={name}
     return (
