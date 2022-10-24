9ba98be98a0cc10a0f7eb21061fe12ded523b384
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-24 14:35:46 +0100

    create AuthContext to manage state

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,21 +4,24 @@ import Navbar from "./Components/Layouts/Navbar";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
 import Footer from "./Components/Layouts/Footer";
+import AuthContextProvider from './Context/AuthContext'
 import Trips from "./Pages/Trips";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/results_availabilities" element={<Trips/>}/>
-        </Routes>
-        <Footer/>
-      </div>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/results_availabilities" element={<Trips />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
---
+++ Client/src/Context/AuthContext.tsx
@@ -0,0 +1,13 @@
+import {createContext} from 'react'
+export const AuthContext=createContext(null)
+
+const AuthContextProvider=({children}:any)=>{
+    const name:string="marouane"
+    const values={name}
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export default AuthContextProvider
