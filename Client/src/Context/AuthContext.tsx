6f58e75f1d6e87b01339a3d2fe6cd3cb800b7347
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-24 14:59:46 +0100

    add some state

--- Client/src/Context/AuthContext.tsx
+++ Client/src/Context/AuthContext.tsx
@@ -1,15 +1,16 @@
-import {createContext} from 'react'
+import { createContext,useState } from "react";
 import { useNavigate } from "react-router-dom";
-export const AuthContext=createContext(null)
+export const AuthContext = createContext(null);
 
-const AuthContextProvider=({children}:any)=>{
-
-    const name:string="marouane"
-    const values={name}
-    return (
-        <AuthContext.Provider value={values}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-export default AuthContextProvider
+const AuthContextProvider = ({ children }: any) => {
+  const navigate = useNavigate();
+  const [connected, setConnected] = useState(localStorage.getItem("logged"));
+  const [id, setId] = useState<string | null>(localStorage.getItem("client_id"));
+  const [success, setSuccess] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const name: string = "marouane";
+  const values = { name };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+export default AuthContextProvider;
