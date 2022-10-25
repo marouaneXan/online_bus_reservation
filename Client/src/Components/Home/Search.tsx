c9beebbdf447b1092d549ac0d69fff2c0170de63
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-25 07:20:16 +0100

    add formik for search trip

--- Client/src/Components/Home/Search.tsx
+++ Client/src/Components/Home/Search.tsx
@@ -2,43 +2,57 @@ import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Formik, Form } from "formik";
 import TextField from "../Formik/TextField";
+import { SearchTripSchema } from "../../Validation/SearchTrip";
 
 const Search = () => {
-  const values:any={
-    departure_city:"",
-    arrival_city:"",
-    departure_date:"",
-  }
+  const values: any = {
+    departure_city: "",
+    arrival_city: "",
+    departure_date: "",
+  };
+  const onSubmit = (values:any) => {
+    alert(JSON.stringify(values))
+  };
+
   return (
-    <form
-      className="flex flex-col space-y-6 justify-between items-center max-w-[800px] mx-auto w-full border p-8
-          rounded-md text-black bg-gray-100/90 md:flex-row md:space-y-0"
+    <Formik
+      initialValues={values}
+      validationSchema={SearchTripSchema}
+      onSubmit={onSubmit}
     >
-      <input
-        className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
-                  "
-        type="text"
-        placeholder="From"
-      />
-      <input
-        className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
-                  "
-        type="text"
-        placeholder="To"
-      />
-      <input
-        className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
-                  "
-        placeholder="Check in"
-      />
-      <button className="p-3 w-full md:w-[50px] border bg-gradient-to-r text-white rounded-md from-[#5651e5] to-[#709dff]">
-        <AiOutlineSearch
-          size={20}
-          className="icon text-2xl cursor-pointer"
-          style={{ color: "#ffffff" }}
-        />
-      </button>
-    </form>
+      {(formik) => (
+        <Form
+          className="flex flex-col space-y-6 space-x-6 justify-between items-center max-w-[800px] mx-auto w-full border py-4 px-8
+          rounded-md text-black bg-gray-100/90 md:flex-row md:space-y-0"
+        >
+          <TextField
+            className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none"
+            name="departure_city"
+            type="text"
+            placeholder="From"
+          />
+          <TextField
+            className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none"
+            name="arrival_city"
+            type="text"
+            placeholder="To"
+          />
+          <TextField
+            className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none"
+            placeholder="Check in"
+            type="date"
+            name="departure_date"
+          />
+          <button type="submit" className="p-3 w-full md:w-[50px] border bg-gradient-to-r text-white rounded-md from-[#5651e5] to-[#709dff]">
+            <AiOutlineSearch
+              size={20}
+              className="icon text-2xl cursor-pointer"
+              style={{ color: "#ffffff" }}
+            />
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
