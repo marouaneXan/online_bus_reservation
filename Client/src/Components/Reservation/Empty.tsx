ec0117a1b74eec31e5278f9714acef349278fc8c
marouaneXan <marouanezahaoui835@gmail.com>
2022-10-30 12:27:43 +0100

    add empty reservation component in reservations page

--- Client/src/Components/Reservation/Empty.tsx
+++ Client/src/Components/Reservation/Empty.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
-const Empty = () => {
+import basket from '../../assets/basket.webp'
+const Empty = ({error}:any) => {
   return (
-    <div>Empty</div>
+    <div className='h-80 flex flex-col space-y-2 justify-center items-center'>
+      <img className="w-full h-full object-cover" src={basket} alt="/" />
+      <p className=' text-xl md:text-2xl font-semibold text-blue-600'>{error}</p>
+    </div>
   )
 }
 
--- Client/src/Components/Reservation/ReservationCards.tsx
+++ Client/src/Components/Reservation/ReservationCards.tsx
@@ -4,15 +4,16 @@ import ReservationDetails from "./ReservationDetails";
 import { Link } from "react-router-dom";
 import { TripContext } from "../../Context/TripContext";
 import Spinner from "../Layouts/Spinner";
+import Empty from "./Empty";
 const ReservationCards = () => {
-  const { reservations, getClientReservations, loading }: any =
+  const { reservations, getClientReservations, loading, error }: any =
     useContext(TripContext);
   const client_id = localStorage.getItem("client_id");
   useEffect(() => {
     getClientReservations(client_id);
   }, [client_id]);
   return (
-    <div className="bg-[rgb(244,243,255)] flex flex-col space-y-6 px-20 py-12 mx-auto justify-center items-center h-full">
+    <div className="bg-[rgb(244,243,255)] flex flex-col space-y-6 px-20 py-12 mx-auto justify-center items-center">
       <div className="flex justify-between items-center w-full">
         <h1 className="font-extrabold text-[37.9444px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
           My Reservations
@@ -29,12 +30,18 @@ const ReservationCards = () => {
         <Spinner />
       ) : (
         <>
-          {reservations?.map((reservation: any) => (
-            <ReservationDetails
-              reservation={reservation}
-              key={reservation._id}
-            />
-          ))}
+          {error ? (
+            <Empty error={error} />
+          ) : (
+            <>
+              {reservations?.map((reservation: any) => (
+                <ReservationDetails
+                  reservation={reservation}
+                  key={reservation._id}
+                />
+              ))}
+            </>
+          )}
         </>
       )}
     </div>
