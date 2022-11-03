430b53a196a7e960ca1ba327ad6a611438493e55
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-03 08:07:21 +0100

    add route for login page

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Layouts/Navbar";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </div>
       </Router>
