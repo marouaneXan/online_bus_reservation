fcfa20aa132d15a9cdbf3156999df6e7dda075e9
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-03 08:02:35 +0100

    setup route for navbar and sidebar components

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Layouts/Navbar";
 
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+        </Routes>
+      </div>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
