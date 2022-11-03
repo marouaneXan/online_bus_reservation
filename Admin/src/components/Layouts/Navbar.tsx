7e0afb82a96af400b173fe281267a3c56ce4403f
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-03 09:40:11 +0100

    create first version for navbar component

--- Admin/src/components/Layouts/Navbar.tsx
+++ Admin/src/components/Layouts/Navbar.tsx
@@ -1,15 +1,126 @@
-import React from 'react'
-import {useLocation} from 'react-router-dom'
+import React from "react";
+import { useLocation,Link } from "react-router-dom";
 
 const Navbar = () => {
-    const location=useLocation()
+  const location = useLocation();
   return (
     <>
-    {location.pathname!=="/login" && (
-        
-        <div>Navbar</div>
-    )}</>
-  )
-}
+      {location.pathname !== "/login" && (
+        <nav className="fixed z-30 w-full bg-gray-50">
+          <div className="py-3 px-3 lg:px-5 lg:pl-3">
+            <div className="flex justify-between items-center">
+              <div className="flex justify-start items-center">
+                <button
+                  aria-expanded="true"
+                  aria-controls="sidebar"
+                  className="p-2 mr-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100"
+                >
+                  <svg
 
-export default Navbar
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <svg
+
+                    className="hidden w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                <Link to="/" className="text-md font-semibold flex items-center lg:mr-1.5">
+                  <span className="hidden md:inline-block self-center text-xl font-bold whitespace-nowrap">
+                    Merkob.ma
+                  </span>
+                </Link>
+                <form
+                  className="hidden lg:block lg:pl-8"
+                >
+                  <label htmlFor="topbar-search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative mt-1 lg:w-80">
+                    <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <svg
+                        className="w-5 h-5 text-gray-500"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      name="email"
+  
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full pl-10 p-2.5"
+                      placeholder="Search"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="flex items-center">
+                {/* <!-- Search mobile --> */}
+                <button
+                  type="button"
+                  className="p-2 text-gray-500 rounded-2xl lg:hidden hover:text-gray-900 hover:bg-gray-100"
+                >
+                  <span className="sr-only">Search</span>
+                  {/* <!-- Search icon --> */}
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                <a
+                  href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-flowbite"
+                  target="_blank"
+                  className="sm:inline-flex ml-5 text-white bg-gradient-to-br from-pink-500 to-voilet-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-3 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                >
+                  <svg
+                    className="mr-2 -ml-1 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+                  </svg>
+                  Upgrade to Pro
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
