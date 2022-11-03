d9c6033710f0e1d1ea1b214e34dee217cd87dbe3
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-03 09:23:48 +0100

    hidd navbar in login page

--- Admin/src/components/Layouts/Navbar.tsx
+++ Admin/src/components/Layouts/Navbar.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import {useLocation} from 'react-router-dom'
 
 const Navbar = () => {
+    const location=useLocation()
   return (
-    <div>Navbar</div>
+    <>
+    {location.pathname!=="/login" && (
+        
+        <div>Navbar</div>
+    )}</>
   )
 }
 
