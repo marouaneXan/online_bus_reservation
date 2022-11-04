0b728422f1c24ffc80ff7eaadea7f1afd9e87a22
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-04 12:07:59 +0100

    create Buses page

---
+++ Admin/src/pages/content/Buses.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReservationCard from "../../components/Reservation/ReservationCard";
+
+const Reservations = () => {
+  return (
+    <div className="flex overflow-hidden bg-white pt-16">
+      <div className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+        <div className="flex flex-col my-6 mx-4 rounded-2xl shadow-xl shadow-gray-200">
+          <div className="overflow-x-auto rounded-2xl">
+            <div className="inline-block min-w-full align-middle">
+              <div className="overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200 table-fixed">
+                  <thead className="bg-white">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Bus name
+                      </th>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Places
+                      </th>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Bus image
+                      </th>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {/* <ReservationCard /> */}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reservations;
