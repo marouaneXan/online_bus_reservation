c6bde096d2b8268afae2f31677370d97178cb852
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-04 10:59:43 +0100

    create trip component

---
+++ Admin/src/components/Trip/TripCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { GrEdit } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
+
+const TripCard = () => {
+  return (
+    <tr className="hover:bg-gray-100">
+      <td className="p-4 text-sm font-normal text-gray-500 whitespace-nowrap lg:p-5">
+        <div className="text-base font-semibold text-gray-900">Safi</div>
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        Casa
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        2022-02-12
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        00:00
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        CTM
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        02:00
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        Safari
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        eljadida marrakech
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        123$
+      </td>
+      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap lg:p-5">
+        123km
+      </td>
+      <td className="p-4 flex items-center justify-between mt-1 space-x-2 whitespace-nowrap lg:p-5">
+        <AiFillDelete className="text-[18px] cursor-pointer" />
+        <GrEdit className="text-[18px] cursor-pointer" />
+      </td>
+    </tr>
+  );
+};
+
+export default TripCard;
