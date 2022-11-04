e06db5f900776f8bff717adc837a162cd5e8a9c4
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-04 14:28:43 +0100

    create companies page

---
+++ Admin/src/pages/content/Companies.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Companies = () => {
+  return (
+    <div className="flex overflow-hidden bg-white pt-16">
+      <div className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+        <div className="flex flex-col my-6 mx-4 rounded-2xl shadow-xl shadow-gray-200">
+          <div className="overflow-x-auto rounded-2xl">
+            <div className="inline-block min-w-full align-middle">
+              <div className="overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200 table-fixed">
+                  <thead className="bg-white">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Company name
+                      </th>
+                      <th
+                        scope="col"
+                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                      >
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {/* <ReservationCard /> */}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Companies;
