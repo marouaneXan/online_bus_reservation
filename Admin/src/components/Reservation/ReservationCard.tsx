ae4ece1f483fc163c6e9db0bedcbd53e5b8abf25
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-04 11:17:52 +0100

    create reservation page

---
+++ Admin/src/components/Reservation/ReservationCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReservationCard = () => {
+  return (
+    <div>ReservationCard</div>
+  )
+}
+
+export default ReservationCard
---
+++ Admin/src/pages/content/Reservations.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reservations = () => {
+  return (
+    <div>Reservations</div>
+  )
+}
+
+export default Reservations
