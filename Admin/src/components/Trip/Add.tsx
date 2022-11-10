28f962eaae8e885a729dcd53c8db3778fd2bdff8
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-10 20:57:45 +0100

    create first version for modal add trip

---
+++ Admin/src/components/Trip/Add.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+
+const Add = () => {
+  return (
+    <div
+      className="flex overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
+    >
+      <div className="relative px-4 w-full max-w-2xl h-full md:h-auto">
+        <div className="relative bg-white rounded-2xl shadow-lg">
+          <div className="flex justify-between items-start p-5 rounded-t border-b">
+            <h3 className="text-xl font-semibold">Add product</h3>
+            <button
+              type="button"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
+              data-modal-toggle="add-product-modal"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div className="p-6 space-y-6">
+            <div>
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="product-name"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Product Name
+                  </label>
+                  <input
+                    type="text"
+                    name="product-name"
+                    id="product-name"
+                    className="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                    placeholder="Apple Imac 27”"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="category"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Category
+                  </label>
+                  <input
+                    type="text"
+                    name="category"
+                    id="category"
+                    className="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                    placeholder="Electronics"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="brand"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Brand
+                  </label>
+                  <input
+                    type="text"
+                    name="brand"
+                    id="brand"
+                    className="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                    placeholder="Apple"
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="price"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Price
+                  </label>
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    className="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                    placeholder="$2300"
+                    required
+                  />
+                </div>
+                <div className="col-span-full">
+                  <label
+                    htmlFor="product-details"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Product Details
+                  </label>
+                  <textarea
+                    id="product-details"
+                    className="block p-4 w-full text-gray-900 border border-gray-300 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300"
+                    placeholder="e.g. 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, Ram 16 GB DDR4 2300Mhz"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-b border-t border-gray-200">
+              <button
+                className="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                type="submit"
+              >
+                Add product
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
