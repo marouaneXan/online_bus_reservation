09024ec0ddfbb1e1bd531fdcdd9cd2422f9e38cc
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-11 19:37:22 +0100

    create validation for input trip

---
+++ Admin/src/Validation/Trip.ts
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+export const Trip = yup.object().shape({
+  departure_city: yup.string().required(),
+  arrival_city: yup.string().required(),
+  departure_date: yup.string().required(),
+  departure_time: yup.string().required(),
+  company: yup.string().required(),
+  arrival_time: yup.string().required(),
+  car: yup.string().required(),
+  break_point: yup.string().required(),
+  price: yup.number().required(),
+  distance: yup.string().required(),
+});
