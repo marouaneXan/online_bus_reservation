a9ead7d537778c08e03e251050c20a4b292f7a8a
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-13 14:02:34 +0100

    check if trip exist

--- Client/src/Components/Reservation/ReservationDetails.tsx
+++ Client/src/Components/Reservation/ReservationDetails.tsx
@@ -13,7 +13,7 @@ const ReservationDetails = ({ reservation }: any) => {
           <span className="text-transparent bg-clip-text bg-gradient-to-r font-bold from-purple-400 to-blue-600">
             GO
           </span>{" "}
-          {reservation.trip.departure_date}
+          {reservation.trip?.departure_date}
         </h3>
         <AiOutlineDelete
           onClick={() => {
@@ -34,15 +34,15 @@ const ReservationDetails = ({ reservation }: any) => {
         <div className="border-r border-dashed border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none p-4 flex flex-col justify-between leading-normal">
           <div className="mb-2">
             <p className="text-sm text-gray-600 bg-gray-200 p-2 rounded text-center">
-              Bus name: {reservation.trip.car.car_name}
+              Bus name: {reservation.trip?.car?.car_name}
             </p>
             <div className="flex space-x-4 md:space-x-6 items-center">
               <div className="flex flex-col md:p-2 mt-5">
                 <h1 className="text-xl md:text-[22px] font-bold text-center">
-                  {reservation.trip.departure_time}
+                  {reservation.trip?.departure_time}
                 </h1>
                 <p className="text-center text-gray-500 font-bold">
-                  {reservation.trip.departure_city}
+                  {reservation.trip?.departure_city}
                 </p>
               </div>
               <div className="inline-flex justify-center items-center w-full">
@@ -53,10 +53,10 @@ const ReservationDetails = ({ reservation }: any) => {
               </div>
               <div className="flex flex-col md:p-2 mt-5">
                 <h1 className="text-xl md:text-[22px] font-bold text-center">
-                  {reservation.trip.arrival_time}
+                  {reservation.trip?.arrival_time}
                 </h1>
                 <p className="text-center text-gray-500 font-bold">
-                  {reservation.trip.arrival_city}
+                  {reservation.trip?.arrival_city}
                 </p>
               </div>
             </div>
@@ -64,15 +64,15 @@ const ReservationDetails = ({ reservation }: any) => {
               <p className="text-center text-gray-500 font-semibold">
                 From{" "}
                 <span className="font-bold text-gray-600">
-                  {reservation.trip.departure_city}
+                  {reservation.trip?.departure_city}
                 </span>{" "}
                 to{" "}
                 <span className="font-bold text-gray-600">
-                  {reservation.trip.arrival_city}
+                  {reservation.trip?.arrival_city}
                 </span>
               </p>
               <p className="text-center text-gray-500 font-semibold">
-                {reservation.trip.distance} KM
+                {reservation.trip?.distance} KM
               </p>
               <p className="text-center text-gray-500 font-semibold">
                 ID: {reservation._id}
@@ -82,7 +82,7 @@ const ReservationDetails = ({ reservation }: any) => {
         </div>
         <div className="border-r border-b border-l border-dashed border-gray-400 lg:border-l-0 lg:border-t lg:rounded-r lg:border-gray-400 bg-white p- md:px-6 md:pt-12 flex flex-col items-center space-y-6 leading-normal">
           <h1 className="text-xl md:text-[22px] font-bold text-center md:mt-10">
-            {reservation.trip.price}DH
+            {reservation.trip?.price}DH
           </h1>
         </div>
       </div>
