5bb60f30b20bad576874759023bb9ab236d63442
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-13 14:24:11 +0100

    add spinner and empty reservation component and get all reservation successfully

--- Admin/src/pages/content/Reservations.tsx
+++ Admin/src/pages/content/Reservations.tsx
@@ -1,74 +1,88 @@
 import React, { useContext } from "react";
+import Spinner from "../../components/Layouts/Spinner/Spinner";
 import ReservationCard from "../../components/Reservation/ReservationCard";
+import Empty from "../../components/Trip/Empty";
 import { ReservationContext } from "../../Context/Reservation";
 import { Reservation } from "../../types";
 
 const Reservations = () => {
-  const { reservations }: any = useContext(ReservationContext);
-  console.log(reservations)
+  const { reservations, loading, empty }: any = useContext(ReservationContext);
   return (
     <div className="flex overflow-hidden bg-white pt-16">
       <div className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
         <div className="flex flex-col my-6 mx-4 rounded-2xl shadow-xl shadow-gray-200">
-          <div className="overflow-x-auto rounded-2xl">
-            <div className="inline-block min-w-full align-middle">
-              <div className="overflow-hidden">
-                <table className="min-w-full divide-y divide-gray-200 table-fixed">
-                  <thead className="bg-white">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Client name
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Reservation date
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Reservation time
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        From
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        To
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Price
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {reservations?.map((reservation:Reservation) => (
-                      <ReservationCard key={reservation._id} reservation={reservation} />
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              {empty ? (
+                <Empty empty={empty} />
+              ) : (
+                <div className="overflow-x-auto rounded-2xl">
+                  <div className="inline-block min-w-full align-middle">
+                    <div className="overflow-hidden">
+                      <table className="min-w-full divide-y divide-gray-200 table-fixed">
+                        <thead className="bg-white">
+                          <tr>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Client name
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Reservation date
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Reservation time
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              From
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              To
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Price
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Actions
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                          {reservations?.map((reservation: Reservation) => (
+                            <ReservationCard
+                              key={reservation._id}
+                              reservation={reservation}
+                            />
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
