f27c90f14ad99030414199deb124cc736963e61b
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-13 13:14:21 +0100

    create function add all reserrvations

---
+++ Admin/src/Context/Reservation/index.tsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useMemo } from "react";
+import axios from "axios";
+import { Bus } from "../../types";
+import { Proxy } from "../../Config/Proxy";
+export const ReservationContext=createContext(null)
+const ReservationContextProvider = ({ children }: any) => {
+  const [reservations, setReservations] = useState<Bus[] | null>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [empty, setEmpty] = useState<boolean>(false);
+  // get all Reservations
+  const getReservation = async () => {
+    setLoading(true);
+    const res = await axios.get(`${Proxy}/reservations`).catch((err) => {
+      const message: any =
+        (err.res && err.res.data && err.res.data.message) || err || err.message;
+      if (message) {
+        setLoading(false);
+        setEmpty(message.response.data.message);
+        setTimeout(() => {
+          setLoading(false);
+        }, 4000);
+      }
+    });
+    if (res && res.data) {
+      setLoading(false);
+      setReservations(res.data);
+    }
+  };
+  const values: any = useMemo(
+    () => ({
+      getReservation,
+      reservations,
+      loading,
+      empty
+    }),
+    [
+      getReservation,
+      reservations,
+      loading,
+      empty
+    ]
+  );
+  return <ReservationContext.Provider value={values} >{children}</ReservationContext.Provider>
+};
+export default ReservationContextProvider;
