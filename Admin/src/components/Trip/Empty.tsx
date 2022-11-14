4628132b406c3a54aafdde03f02aadc48fb8593d
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-14 13:45:08 +0100

    show modal

--- Admin/src/components/Trip/Empty.tsx
+++ Admin/src/components/Trip/Empty.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{useContext} from "react";
 import { useLocation } from "react-router-dom";
+import { BusContext } from "../../Context/Car";
 interface Prop {
   empty: string;
   content?: string;
 }
 
 const Empty = (props: Prop) => {
+  const {displayModalAdd}:any=useContext(BusContext)
   const location = useLocation();
   return (
     <div className="h-[32rem] flex flex-col space-y-2 justify-center items-center">
@@ -13,7 +15,7 @@ const Empty = (props: Prop) => {
         {props.empty}
       </p>
       {location.pathname === "/buses" && (
-        <button className="my-1 w-[130px] text-white bg-emerald-500 bg-gradient-to-r from-[#5651e5] to-[#709dff] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:scale-[1.02] transition-transform">
+        <button onClick={displayModalAdd} className="my-1 w-[130px] text-white bg-emerald-500 bg-gradient-to-r from-[#5651e5] to-[#709dff] font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:scale-[1.02] transition-transform">
           Add new {props.content}
         </button>
       )}
