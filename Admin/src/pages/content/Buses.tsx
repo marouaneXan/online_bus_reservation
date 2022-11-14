887d7cf7e8ce80473e757b34f348cfa3852a1642
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-14 11:01:50 +0100

    add spinner and message empty buses

--- Admin/src/pages/content/Buses.tsx
+++ Admin/src/pages/content/Buses.tsx
@@ -2,9 +2,11 @@ import React, { useContext, useEffect } from "react";
 import BusCard from "../../components/Bus/BusCard";
 import { Bus } from "../../types";
 import { BusContext } from "../../Context/Car";
+import Spinner from "../../components/Layouts/Spinner/Spinner";
+import Empty from "../../components/Trip/Empty";
 
 const Buses = () => {
-  const { buses, getBuses }: any = useContext(BusContext);
+  const { buses, getBuses, loading, empty }: any = useContext(BusContext);
   useEffect(() => {
     getBuses();
   }, []);
@@ -13,47 +15,57 @@ const Buses = () => {
     <div className="flex overflow-hidden bg-white pt-16">
       <div className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
         <div className="flex flex-col my-6 mx-4 rounded-2xl shadow-xl shadow-gray-200">
-          <div className="overflow-x-auto rounded-2xl">
-            <div className="inline-block min-w-full align-middle">
-              <div className="overflow-hidden">
-                <table className="min-w-full divide-y divide-gray-200 table-fixed">
-                  <thead className="bg-white">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Bus name
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Places
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Bus image
-                      </th>
-                      <th
-                        scope="col"
-                        className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
-                      >
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {buses?.map((bus:Bus) => (
-                      <BusCard key={bus._id} bus={bus}  />
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              {empty ? (
+                <Empty empty={empty} />
+              ) : (
+                <div className="overflow-x-auto rounded-2xl">
+                  <div className="inline-block min-w-full align-middle">
+                    <div className="overflow-hidden">
+                      <table className="min-w-full divide-y divide-gray-200 table-fixed">
+                        <thead className="bg-white">
+                          <tr>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Bus name
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Places
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Bus image
+                            </th>
+                            <th
+                              scope="col"
+                              className="p-4 text-xs font-medium text-left text-gray-500 uppercase lg:p-5"
+                            >
+                              Actions
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                          {buses?.map((bus: Bus) => (
+                            <BusCard key={bus._id} bus={bus} />
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
