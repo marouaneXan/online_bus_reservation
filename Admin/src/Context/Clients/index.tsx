7434b8a571bb867eaa31082e5a49b9fbd7b2670e
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-14 10:07:55 +0100

    delete client

--- Admin/src/Context/Clients/index.tsx
+++ Admin/src/Context/Clients/index.tsx
@@ -2,6 +2,7 @@ import { createContext, useState, useMemo, useEffect } from "react";
 import axios from "axios";
 import { Client } from "../../types";
 import { Proxy } from "../../Config/Proxy";
+import { toast } from "react-toastify";
 export const ClientContext = createContext(null);
 const ClientContextProvider = ({ children }: any) => {
   const [clients, setClients] = useState<Client[] | null>();
@@ -24,17 +25,33 @@ const ClientContextProvider = ({ children }: any) => {
       setClients(res.data);
     }
   };
-  useEffect(()=>{
-    getClients()
-  },[])
+  //delete Bus
+  const deleteClient = async (client_id: string) => {
+    const res = await axios.delete(`${Proxy}/clients/${client_id}`).catch((err) => {
+      const message: any =
+        (err.res && err.res.data && err.res.data.message) || err || err.message;
+      if (message) {
+        toast.error(message.response.data.message);
+        setLoading(false);
+      }
+    });
+    if (res && res.data) {
+      getClients();
+      toast.success(res.data.message);
+    }
+  };
+  useEffect(() => {
+    getClients();
+  }, []);
   const values: any = useMemo(
     () => ({
       getClients,
       clients,
       loading,
       empty,
+      deleteClient,
     }),
-    [getClients, clients, loading, empty]
+    [getClients, clients, loading, empty, deleteClient]
   );
   return (
     <ClientContext.Provider value={values}>{children}</ClientContext.Provider>
--- Admin/src/components/Clients/ClientCard.tsx
+++ Admin/src/components/Clients/ClientCard.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React,{useContext} from "react";
 import { GrEdit } from "react-icons/gr";
 import { AiFillDelete } from "react-icons/ai";
 import { Client } from "../../types";
+import { ClientContext } from "../../Context/Clients";
 interface Prop {
   client: Client;
 }
 
 const ClientCard = (props: Prop) => {
+  const {deleteClient}:any=useContext(ClientContext)
   return (
     <tr className="hover:bg-gray-100">
       <td className="p-4 text-sm font-normal text-gray-500 whitespace-nowrap lg:p-5">
@@ -39,7 +41,7 @@ const ClientCard = (props: Prop) => {
         {props.client.nationalite}
       </td>
       <td className="p-4 flex items-center mt-1 space-x-2 whitespace-nowrap lg:p-5">
-        <AiFillDelete className="text-[18px] cursor-pointer" />
+        <AiFillDelete onClick={()=>{deleteClient(props.client._id)}} className="text-[18px] cursor-pointer" />
       </td>
     </tr>
   );
