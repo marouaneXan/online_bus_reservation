37f911e44275a7e796f61b08c03d412836b4db82
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-15 21:20:00 +0100

    remove search input

--- Admin/src/components/Layouts/Navbar.tsx
+++ Admin/src/components/Layouts/Navbar.tsx
@@ -52,33 +52,6 @@ const Navbar = () => {
                     </Link>
                   </span>
                 </Link>
-                <form className="hidden lg:block lg:pl-8">
-                  <label htmlFor="topbar-search" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative mt-1 lg:w-80">
-                    <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                      <svg
-                        className="w-5 h-5 text-gray-500"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
-                    <input
-                      type="text"
-                      name="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full pl-10 p-2.5"
-                      placeholder="Search"
-                    />
-                  </div>
-                </form>
               </div>
               <div className="flex items-center">
                 {/* <!-- Search mobile --> */}
