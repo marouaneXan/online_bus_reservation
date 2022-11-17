d397fcbcad8d6ed682480e66781236a04c6b78e8
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-17 14:47:36 +0100

    redirect user to register if he want to navogate to reservations page

--- Client/src/Pages/Reservations.tsx
+++ Client/src/Pages/Reservations.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import ReservationCards from '../Components/Reservation/ReservationCards'
+import { token } from '../Config/Token'
 
 const Reservations = () => {
+  const navigate=useNavigate()
+  useEffect(()=>{
+    token ? navigate('/reservations') : navigate('/register')
+  },[token])
   return (
     <>
       <ReservationCards/>
