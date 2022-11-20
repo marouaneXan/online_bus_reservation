c050ab515897b96c2de3821100e28d217590fbc5
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-21 00:02:52 +0100

    add errors message

--- Admin/src/components/Formik/TextField.tsx
+++ Admin/src/components/Formik/TextField.tsx
@@ -11,7 +11,7 @@ interface Prop  {
 const TextField = (props: Prop) => {
   const [field, meta] = useField(props.label ?? "");
   return (
-    <>
+    <div className="w-full">
       <label
         htmlFor={field.name}
         className="block mb-2 text-sm font-medium text-gray-900"
@@ -21,14 +21,14 @@ const TextField = (props: Prop) => {
       <input
         {...field}
         {...props}
-        className={`shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5 ${
-          (meta.touched && meta.error) ?? "border-red-500"
-        }`}
+        // className={`shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5 ${
+        //   (meta.touched && meta.error) ?? "border-red-500"
+        // }`}
       />
-      {/* <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-        <ErrorMessage name={field.name} />
-      </span> */}
-    </>
+      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+        <ErrorMessage name={props.name} />
+      </span>
+    </div>
   );
 };
 
