45ed4f438f26b14f9ea0b55737ebbe39aa7a9e24
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-21 00:05:00 +0100

    add style for inputs

--- Admin/src/components/Formik/TextField.tsx
+++ Admin/src/components/Formik/TextField.tsx
@@ -21,9 +21,9 @@ const TextField = (props: Prop) => {
       <input
         {...field}
         {...props}
-        // className={`shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5 ${
-        //   (meta.touched && meta.error) ?? "border-red-500"
-        // }`}
+        className={`shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5 ${
+          (meta.touched && meta.error) ?? "border-red-500"
+        }`}
       />
       <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
         <ErrorMessage name={props.name} />
