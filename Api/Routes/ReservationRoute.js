846f4a84d0cdd4df039e990e3fc071ad3d8dcd60
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-21 00:19:02 +0100

    remove authorization

--- Api/Routes/ReservationRoute.js
+++ Api/Routes/ReservationRoute.js
@@ -14,12 +14,12 @@ router.get("/statistics", statistic);
 router.get("/", getAllReservations);
 router.get("/:client_id", getClientReservations);
 router.get("/:reservation_id", getReservationDetails);
-// router.post("/:trip_id/:client_id", makeReservation);
+router.post("/:trip_id/:client_id", makeReservation);
 router.delete("/:reservation_id/:trip_id/:client_id", cancelReservation);
 
 // router.get('/',protectAdmin,getAllReservations)
 // router.get('/:client_id', protectClient,getClientReservations)
 // router.get('/:reservation_id', protectAdmin,getReservationDetails)
-router.post('/:trip_id/:client_id',protectClient,makeReservation)
+// router.post('/:trip_id/:client_id',protectClient,makeReservation)
 // router.delete('/:reservation_id/:trip_id/:client_id',protectClient,cancelReservation)
 module.exports = router;
